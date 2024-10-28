51ef069a1d6c14905895d45b5a19c4d0a07e1437
Tino Santoso <umar.ibnu29@gmail.com>
2024-10-28 16:41:45 +0700

    Add: Product Factory & seeder

--- database/migrations/2024_10_26_024345_create_products_table.php
+++ database/migrations/2024_10_26_024345_create_products_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('product_name');
-            $table->text('description');
+            $table->string('category');
+            $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->timestamps();
         });
